f013edd4108b81004df59f1a6bffa99debd29fe5
Renato EM <leo10m2010@hotmail.com>
2020-08-24 20:57:22 -0500

    aa

---
+++ server.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+function presence(){
+    client.user.setPresence({
+       status: "online",
+       activity: {
+          name: "Fall Guys",
+          type: "PLAYING"
+       }
+    });
+ }
+client.on('ready', () => {
+  console.log("Estoy listo!");
+  presence();
+});
+
+
+client.on("guildMemberAdd", miembro =>{
+    var Canal = client.channels.cache.find(channel => channel.id === ("747288908584452216"));
+    Canal.send("Bienvenido <@" + miembro.id + "> al servidor, recuerda hablar con respeto.\n\nEsperemos que la pases bien :). :stuck_out_tongue_winking_eye: ");
+ });
+
+client.on('message', msg => {
+  if (msg.content === '.hp') {
+    msg.reply('Feliz Cumpleaños Negro :pato:');
+  }
+});
+
+client.login('NzQ3MjgzNzc2Mjc4NjI2MzU1.X0MoQA.l38Gd_BKLnLBowf5wfsO6HvSwgY');
